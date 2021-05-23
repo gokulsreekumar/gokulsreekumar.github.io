3a85c5ce6dded5261ecef65cd10b42ddcab8ac8a
gokulsreekumar <gokulsreekumar2@gmail.com>
2021-05-23 18:47:47 +0530

    Design Updates
    - New Accent Picker
    - New Sidebar Menu

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__item');
+
+const accents = [document.querySelector('.nav__color_btn1'), document.querySelector('.nav__color_btn2'), document.querySelector('.nav__color_btn3')];
+
 console.log(navLinks.length);
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -7,7 +10,18 @@ navToggle.addEventListener('click', () => {
 
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
-        console.log("Im here");
+        // console.log("Im here");
         document.body.classList.remove('nav-open');
     })
 });
+
+
+accents.forEach((accent, index) => {
+    accent.addEventListener('click', () => {
+        // console.log(`here${index}`);
+        document.body.classList.remove('accent1');
+        document.body.classList.remove('accent2');
+        document.body.classList.remove('accent3');
+        document.body.classList.toggle(`accent${index + 1}`);
+    })
+})
